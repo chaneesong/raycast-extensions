100c39d96a21b85d26874d89c3f41a27fab5f68f
chaneesong <sch102403@gmail.com>
2023-09-04 03:00:56 +0900

    feat: alias 추가

--- search-browser/src/search-browser.tsx
+++ search-browser/src/search-browser.tsx
@@ -1,10 +1,22 @@
 import { LaunchProps, closeMainWindow, open, popToRoot } from "@raycast/api";
 
+const SEARCH_ALIAS: { [key: string]: string } = {
+  gpt: "https://chat.openai.com/",
+  youtube: "https://www.youtube.com/",
+  github: "https://github.com/chaneesong?tab=repositories&type=source",
+  naver: "https://www.naver.com/",
+  tving: "https://www.tving.com/",
+};
+
 export default async function Command(props: LaunchProps<{ arguments: Arguments.SearchBrowser }>) {
   const { url } = props.arguments;
+  let alias;
+  if (SEARCH_ALIAS[url]) {
+    alias = SEARCH_ALIAS[url];
+  }
   const startsWithHttps = /^https:\/\//.test(url);
   const fullUrl = startsWithHttps ? url : `https://${url}`;
-  await open(fullUrl);
+  await open(alias ? alias : fullUrl);
   await popToRoot({ clearSearchBar: true });
   await closeMainWindow({ clearRootSearch: true });
 
