9282046c1f467c205238a43555d047cd6c57362c
chaneesong <sch102403@gmail.com>
2023-09-17 13:01:14 +0900

    youtube gcp link 추가

---
+++ open-gcp-console.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open GCP Console
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Open Browser
+
+# Documentation:
+# @raycast.description open gcp console
+# @raycast.author sch102403
+# @raycast.authorURL https://raycast.com/sch102403
+
+open https://console.cloud.google.com/?hl=ko
---
+++ open-github.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open GITHUB
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Open Browser
+
+# Documentation:
+# @raycast.description open github
+# @raycast.author sch102403
+# @raycast.authorURL https://raycast.com/sch102403
+
+open https://github.com/chaneesong
