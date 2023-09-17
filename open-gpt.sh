44b37352e7a4bf5d3422d426fcb89488a7f6115c
chaneesong <sch102403@gmail.com>
2023-09-17 12:42:23 +0900

    웹 링크를 여는 스크립트 추가

---
+++ open-gpt.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open GPT
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Open Browser
+
+# Documentation:
+# @raycast.description Open GPT
+# @raycast.author sch102403
+# @raycast.authorURL https://raycast.com/sch102403
+
+open https://chat.openai.com/
