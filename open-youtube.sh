0730378c6533721d988798d8ae727a676c68879d
chaneesong <sch102403@gmail.com>
2023-10-16 11:32:43 +0900

    youtube 추가

---
+++ open-youtube.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Youtube
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+# @raycast.packageName Script Commands
+
+# Documentation:
+# @raycast.description open youtube
+# @raycast.author sch102403
+# @raycast.authorURL https://raycast.com/sch102403
+
+open https://www.youtube.com/
