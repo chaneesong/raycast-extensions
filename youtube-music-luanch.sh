30f2421e95ed1f06b3df41737c88b6f4f38f3ee1
chaneesong <sch102403@gmail.com>
2023-10-27 10:08:55 +0900

    backup

---
+++ youtube-music-luanch.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Youtube Music Luanch
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+# Documentation:
+# @raycast.description youtube music luanch
+# @raycast.author sch102403
+# @raycast.authorURL https://raycast.com/sch102403
+
+xattr -cr /Applications/YouTube\ Music.app
+
